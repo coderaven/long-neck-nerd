f61251904c3b5bc3a8f53e7d45308988c72509ed
Raven Duran <rabintoy@gmail.com>
2016-08-21 23:05:11 +0800

    Added Template for RecyclerView and Packages needed

--- LongNeckNerd/MainActivity.cs
+++ LongNeckNerd/MainActivity.cs
@@ -1,12 +1,14 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.Widget;
 
 namespace LongNeckNerd
 {
 	[Activity(Label = "Long Neck Nerd", Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
+		RecyclerView recycleViewAttractions;
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -14,6 +16,7 @@ namespace LongNeckNerd
 
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
+			recycleViewAttractions = FindViewById<RecyclerView>(Resource.Id.recyclerViewAttractions);
 		}
 	}
 }
